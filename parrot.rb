1caa1a127814402fc60940563e6de8f0eb8cd3e6
Michael Bade <mbade314@gmail.com>
2020-05-11 16:07:02 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,4 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase = "Squawk")
+def parrot(string = "Squawk")
+  return_item = "#{string}!"
+  return return_item
 end
